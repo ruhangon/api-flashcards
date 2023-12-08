ce770ba0aa8ce8acd88963c1a4f6f1790a9daaa1
ruhangon <ruhan.goncalves@gmail.com>
2023-12-08 17:23:13 -0300

    feat: inicia projeto de api

---
+++ src/test/java/br/com/example/apiflashcards/ApiflashcardsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.example.apiflashcards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiflashcardsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
