725ad10c7d882cdfc4c47411c7e3bdc9b0b32498
ruhangon <ruhan.goncalves@gmail.com>
2023-12-10 18:19:50 -0300

    feat: implementa a repository, a service e a mapper para cadastro de baralho

---
+++ src/main/java/br/com/example/apiflashcards/service/CadastroDeBaralhoService.java
@@ -0,0 +1,8 @@
+package br.com.example.apiflashcards.service;
+
+import br.com.example.apiflashcards.model.dto.CadastroDeBaralhoDTO;
+
+public interface CadastroDeBaralhoService {
+	String cadastrarBaralho(CadastroDeBaralhoDTO cadastroDeBaralhoDTO);
+
+}
--- src/main/java/br/com/example/apiflashcards/service/impl/CadastroDeBaralhoServiceImpl.java
+++ src/main/java/br/com/example/apiflashcards/service/impl/CadastroDeBaralhoServiceImpl.java
@@ -1,14 +1,31 @@
-package br.com.example.apiflashcards.service;
+package br.com.example.apiflashcards.service.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import br.com.example.apiflashcards.mapper.BaralhoMapper;
+import br.com.example.apiflashcards.model.Baralho;
 import br.com.example.apiflashcards.model.dto.CadastroDeBaralhoDTO;
+import br.com.example.apiflashcards.repository.BaralhoRepository;
+import br.com.example.apiflashcards.service.CadastroDeBaralhoService;
 
 @Service
-public class CadastroDeBaralhoService {
+public class CadastroDeBaralhoServiceImpl implements CadastroDeBaralhoService {
+	private final BaralhoRepository baralhoRepository;
 
+	@Autowired
+	public CadastroDeBaralhoServiceImpl(BaralhoRepository baralhoRepository) {
+		this.baralhoRepository = baralhoRepository;
+	}
+
+	@Override
+	@Transactional
 	public String cadastrarBaralho(CadastroDeBaralhoDTO cadastroDeBaralhoDTO) {
+		BaralhoMapper baralhoMapper = new BaralhoMapper();
+		Baralho baralho = baralhoMapper.toBaralho(cadastroDeBaralhoDTO);
+		baralhoRepository.save(baralho);
 		return HttpStatus.CREATED.getReasonPhrase();
 	}
 
