73a37c78f1d0ba71f854eed2fdd4fae6ed52f095
ruhangon <ruhan.goncalves@gmail.com>
2023-12-24 21:45:33 -0300

    feat: implementa as entidades para persistência no banco de dados

---
+++ src/main/java/br/com/example/apiflashcards/adaptador/saida/entity/CartaEntity.java
@@ -0,0 +1,72 @@
+package br.com.example.apiflashcards.adaptador.saida.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "carta")
+public class CartaEntity {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String frente;
+	private String tras;
+	private Integer fila;
+
+	@ManyToOne
+	@JoinColumn(name = "baralho_id")
+	private BaralhoEntity baralho;
+
+	public CartaEntity(String frente, String tras, Integer fila, BaralhoEntity baralhoEntity) {
+		this.frente = frente;
+		this.tras = tras;
+		this.fila = fila;
+		this.baralho = baralhoEntity;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFrente() {
+		return frente;
+	}
+
+	public void setFrente(String frente) {
+		this.frente = frente;
+	}
+
+	public String getTras() {
+		return tras;
+	}
+
+	public void setTras(String tras) {
+		this.tras = tras;
+	}
+
+	public Integer getFila() {
+		return fila;
+	}
+
+	public void setFila(Integer fila) {
+		this.fila = fila;
+	}
+
+	public BaralhoEntity getBaralho() {
+		return baralho;
+	}
+
+	public void setBaralho(BaralhoEntity baralho) {
+		this.baralho = baralho;
+	}
+
+}
