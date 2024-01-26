1c914ba23458a57ebc5b7f08ea752e884d5461a1
ruhangon <ruhan.goncalves@gmail.com>
2024-01-25 23:17:42 -0300

    feat: implementa novo método para a service de cadastro de carta, para cadastrar uma carta em um baralho existente

--- src/main/java/br/com/example/apiflashcards/domain/ports/in/CadastroDeCartaServicePorta.java
+++ src/main/java/br/com/example/apiflashcards/domain/ports/in/CadastroDeCartaServicePorta.java
@@ -2,9 +2,12 @@ package br.com.example.apiflashcards.domain.ports.in;
 
 import java.util.List;
 
+import br.com.example.apiflashcards.adapter.in.model.dto.InformacaoParaCadastroDeCartaRequestDTO;
 import br.com.example.apiflashcards.adapter.in.model.dto.TextoCartaRequestDTO;
 
 public interface CadastroDeCartaServicePorta {
 	String cadastrarCartas(List<TextoCartaRequestDTO> cartas, Long idBaralho);
+
+	String cadastrarCarta(InformacaoParaCadastroDeCartaRequestDTO informacaoParaCadastroDeCartaDTO, Long idBaralho);
 
 }
